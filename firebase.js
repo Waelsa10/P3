fae1e63bcfa9c9dbc836089f47a17b2e92bac8dc
waelsa10 <waelsa10@hotmail.com>
2025-11-10 14:20:40 +0300

    up

--- firebase.js
+++ firebase.js
@@ -2,11 +2,9 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore, serverTimestamp, doc, setDoc } from "firebase/firestore";
 
-
 const firebaseConfig = {
   apiKey: "AIzaSyCK7AV4BHrj0o_sPhstug4ph59adWv1eb0",
   authDomain: "file-transfer-app-9f7bd.firebaseapp.com",
-  databaseURL: "https://file-transfer-app-9f7bd-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "file-transfer-app-9f7bd",
   storageBucket: "file-transfer-app-9f7bd.firebasestorage.app",
   messagingSenderId: "31409683737",
